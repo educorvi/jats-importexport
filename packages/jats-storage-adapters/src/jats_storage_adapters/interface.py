c937bdd77e5d0e8cc024d01cb9f1314193087e17
Julian Pollinger <julian@pollinger.dev>
2026-05-27 15:53:29 +0200

    feat: init storage adapter package

---
+++ packages/jats-storage-adapters/src/jats_storage_adapters/interface.py
@@ -0,0 +1,21 @@
+import abc
+from typing import BinaryIO
+
+from jats_classes import JATSDocument
+
+
+class StorageAdapter(metaclass=abc.ABCMeta):
+
+   # Returns the URL of the uploaded file
+   @abc.abstractmethod
+   def upload_file(self, file: BinaryIO) -> str:
+      raise NotImplementedError
+
+   @abc.abstractmethod
+   def get_jats_document(self, doi: str) -> JATSDocument:
+      raise NotImplementedError
+
+   #  Returns the URL of the saved file
+   @abc.abstractmethod
+   def save_jats_document(self, document: JATSDocument) -> str:
+       raise NotImplementedError
