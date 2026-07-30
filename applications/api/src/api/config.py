dd0ddcadc9448e4e621a76cb5051e3be7a049e24
jonashuettinger <jonas.huettinger@educorvi.de>
2026-07-30 15:21:55 +0200

    chore: update version

--- applications/api/src/api/config.py
+++ applications/api/src/api/config.py
@@ -19,7 +19,7 @@ class APIConfig:
         "An API for uploading JATS documents, importing them to different storage backends"
         " (e.g. Plone), and converting them to various formats."
     )
-    API_VERSION: str = "1.0.0a10"
+    API_VERSION: str = "1.0.0a11"
 
 
 class StorageConfig:
