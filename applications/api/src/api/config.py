938a31b03aa81ded3ecbd0dd3402b116bada222d
Julian Pollinger <julian@pollinger.dev>
2026-08-04 15:09:03 +0200

    chore: bump version

--- applications/api/src/api/config.py
+++ applications/api/src/api/config.py
@@ -19,7 +19,7 @@ class APIConfig:
         "An API for uploading JATS documents, importing them to different storage backends"
         " (e.g. Plone), and converting them to various formats."
     )
-    API_VERSION: str = "1.0.0a11"
+    API_VERSION: str = "1.0.0a12"
 
 
 class StorageConfig:
