bc0de0e1b225383531ad4b4b1108d1192321b87c
Julian Pollinger <julian@pollinger.dev>
2026-08-20 15:49:39 +0200

    chore: update version and client

--- packages/api-client/jats_importexport_client/api/__init__.py
+++ packages/api-client/jats_importexport_client/api/__init__.py
@@ -4,4 +4,5 @@
 from jats_importexport_client.api.export_api import ExportApi
 from jats_importexport_client.api.status_api import StatusApi
 from jats_importexport_client.api.upload_api import UploadApi
+from jats_importexport_client.api.list_api import ListApi
 
