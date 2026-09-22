22625d77a506c25683335e175da269eea6b01519
Julian Pollinger <julian@pollinger.dev>
2026-09-22 18:20:55 +0200

    chore: version

--- applications/api/src/api/config.py
+++ applications/api/src/api/config.py
@@ -24,7 +24,7 @@ class APIConfig:
         "An API for uploading JATS documents, importing them to different storage backends"
         " (e.g. Plone), and converting them to various formats."
     )
-    API_VERSION: str = "1.0.0a41"
+    API_VERSION: str = "1.0.0a42"
 
 
 if os.environ.get("REDIS_HOST") is not None:
